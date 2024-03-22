0a5d799c7205fae2483590eef8ca18d9b208406c
Drifa <135760500+Mlmdrifa@users.noreply.github.com>
2024-03-22 15:43:45 +0100

    change navigation background with usetheme

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
 import Navigation from '@/navigation/Navigation';
+import { ThemeProvider } from '@/providers/ThemeProvider';
 
 export default function App() {
   return (
     <View style={StyleSheet.absoluteFill}>
       <StatusBar style="auto" />
-      <Navigation />
+      <ThemeProvider>
+        <Navigation />
+      </ThemeProvider>
     </View>
   );
 }
