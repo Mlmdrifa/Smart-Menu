d246b6946ffadc8c6e39898ea0ee788f1165a57f
Drifa <135760500+Mlmdrifa@users.noreply.github.com>
2024-03-22 14:05:33 +0100

    run lint command

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
+
 import Navigation from "./src/navigation/Navigation";
 
 export default function App() {
