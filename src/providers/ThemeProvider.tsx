b3de55a9f7355379ed100689530243119e4c72b6
Drifa <135760500+Mlmdrifa@users.noreply.github.com>
2024-03-24 16:03:27 +0100

    upadate theme by adding mode light mode or dark mode

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,6 +1,8 @@
 import { PropsWithChildren, createContext } from 'react';
 import { useColorScheme, ColorSchemeName } from 'react-native';
 
+export type Theme = { background: string; text: string; mode: ColorSchemeName };
+
 export const ThemeContext = createContext<ColorSchemeName>('light');
 
 export const ThemeProvider = ({ children }: PropsWithChildren) => {
