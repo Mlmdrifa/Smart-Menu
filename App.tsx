cb3f89c2f8e655e99e2940e06fa3f582d5304839
Drifa <135760500+Mlmdrifa@users.noreply.github.com>
2024-03-25 11:29:32 +0100

    inject translations in App

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import '@/translations';
+
 import { fonts } from '@/designs/fonts';
 import Navigation from '@/navigation/Navigation';
 import { ThemeProvider } from '@/providers/ThemeProvider';
