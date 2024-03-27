a18b53e9c152c7a425ab9efc5c4694febc8c5e4e
Drifa <135760500+Mlmdrifa@users.noreply.github.com>
2024-03-27 11:50:39 +0100

    add EmailForm to EmaiVerificationScreen and update screen

--- src/screens/EmailVerificationScreen.tsx
+++ src/screens/EmailVerificationScreen.tsx
@@ -3,9 +3,9 @@ import { useTranslation } from 'react-i18next';
 
 import ScreenWrapper from '@/components/ScreenWrapper';
 import Box from '@/components/atoms/Box';
+import Spacer from '@/components/atoms/Spacer';
 import Typo from '@/components/atoms/Typo';
-import Button from '@/components/molecules/Button';
-import Input from '@/components/molecules/Input';
+import EmailForm from '@/components/organisms/EmailForm';
 
 export default function EmailVerificationScreen() {
   const navigation = useNavigation();
@@ -15,17 +15,14 @@ export default function EmailVerificationScreen() {
     <ScreenWrapper>
       <Box ycenter flex gap={40}>
         <Box center gap={14}>
+          <Spacer vertical={72} />
           <Typo variant="h2">{t('screen.emailVerification.title')}</Typo>
           <Typo center variant="body1">
             {t('screen.emailVerification.description')}
           </Typo>
         </Box>
-        <Input placeholder="Email" />
+        <EmailForm />
       </Box>
-      <Box flex />
-      <Button onPress={navigation.goBack} variant="primary">
-        {t('screen.emailVerification.cta')}
-      </Button>
     </ScreenWrapper>
   );
 }
