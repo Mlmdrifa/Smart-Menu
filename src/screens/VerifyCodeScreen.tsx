fc49c26349f5335acfe1aa8781ea5190df0cb28f
Drifa <135760500+Mlmdrifa@users.noreply.github.com>
2024-03-29 13:28:43 +0100

    Add AuthMethodItem to VerifyCodeScreen

--- src/screens/VerifyCodeScreen.tsx
+++ src/screens/VerifyCodeScreen.tsx
@@ -1,32 +1,44 @@
-import { RouteProp, useRoute } from '@react-navigation/native';
+import { RouteProp, useRoute, useNavigation } from '@react-navigation/native';
+import { Pressable } from 'react-native';
 
 import ScreenWrapper from '@/components/ScreenWrapper';
 import Box from '@/components/atoms/Box';
+import Icon from '@/components/atoms/Icon';
 import Spacer from '@/components/atoms/Spacer';
 import Typo from '@/components/atoms/Typo';
 import AuthMethodItem from '@/components/molecules/AuthMethodItem';
 import Button from '@/components/molecules/Button';
+import { useTheme } from '@/hooks/useTheme';
 import { PublicStackParamList } from '@/navigation/stacks/PublicStack';
 
 export default function VerifyCodeScreen() {
+  const theme = useTheme();
+  const navigation = useNavigation();
+
   const route = useRoute<RouteProp<PublicStackParamList, 'VerifyCode'>>();
   const { email, phoneNumber } = route.params;
   const onSubmit = () => {};
 
   return (
     <ScreenWrapper>
-      <Spacer vertical={70} />
+      <Spacer vertical={16} />
+      <Pressable onPress={navigation.goBack}>
+        <Icon iconName="ArrowLeft" stroke={theme.text} />
+      </Pressable>
+      <Spacer vertical={50} />
       <Box flex>
         <Box gap={14}>
           <Typo center variant="h2">
-            ️Verify Code ⚡️
+            Verify Code ⚡️
           </Typo>
           <Typo center variant="body2">
             Select where do you want to receive the verification code
           </Typo>
-          <Spacer vertical={60} />
-
-          <AuthMethodItem email={email} phoneNumber={phoneNumber} />
+          <Spacer vertical={40} />
+          <Box gap={24}>
+            <AuthMethodItem title="Via email" value={email} onPress={() => {}} />
+            <AuthMethodItem title="Via SMS" value={phoneNumber} onPress={() => {}} />
+          </Box>
         </Box>
         <Box flex />
 
