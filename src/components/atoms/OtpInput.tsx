13fcebf76e26c0b1baff8720278c10b4b185c22b
Drifa <135760500+Mlmdrifa@users.noreply.github.com>
2024-04-02 16:01:32 +0200

    add OtpInput

---
+++ src/components/atoms/OtpInput.tsx
@@ -0,0 +1,32 @@
+import React, { useMemo } from 'react';
+import { OtpInput, OtpInputProps } from 'react-native-otp-entry';
+
+import { colors } from '@/designs/colors';
+import { useTheme } from '@/hooks/useTheme';
+
+type OTPProps = Pick<OtpInputProps, 'onTextChange' | 'onFilled'> & { error?: boolean };
+
+export default function OTPInput({ onTextChange, onFilled, error }: OTPProps) {
+  const theme = useTheme();
+
+  const otpTheme = useMemo(() => {
+    return {
+      pinCodeContainerStyle: {
+        height: 54,
+        width: 54,
+        backgroundColor: theme.mode === 'light' ? colors.n0 : colors.n700,
+        borderWidth: 1,
+      },
+    };
+  }, [theme]);
+
+  return (
+    <OtpInput
+      numberOfDigits={4}
+      onTextChange={onTextChange}
+      autoFocus
+      onFilled={onFilled}
+      theme={otpTheme}
+    />
+  );
+}
